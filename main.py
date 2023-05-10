8c38901a86f22d5fe887fae336dcfc4e694ffa4d
SaatwikCodz <107233799+SaatwikCodz@users.noreply.github.com>
2023-05-10 23:42:46 +0530

    Complete Errorless Base Set up

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from customtkinter import *
-import time
+import customtkinter
+import pyautogui as pag
 from PIL import Image
+flag = ''
+w,h = pag.size()
+customtkinter.set_appearance_mode("Dark")
 def Landing():
+    global w,h
     win1 = CTk()
     old = Image.open('Assets\\Wallpaper.jpg')
     new_image = old.resize((w, h))
     new_image.save('Assets\\Wallpaper.jpg')
-    background = CTkImage(light_image = Image.open('Assets\\Wallpaper.jpg'),
-                        size=(w, h))   
+    background = customtkinter.CTkImage(light_image = Image.open('Assets\\Wallpaper.jpg'),
+                        dark_image = Image.open('Assets\\Wallpaper.jpg'),
+                        size=(w, h))
+    back = CTkLabel(win1,text = ' ', image = background).pack()
+    win1.attributes('-fullscreen',True)
     win1.mainloop()
 Landing()
